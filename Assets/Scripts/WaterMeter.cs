2a1c697175e563ba343c8881ad5d245af74fbab7
Katelyn Watkins <kate_watkins101@yahoo.com>
2016-01-11 17:51:51 -0600

    another commit

---
+++ Assets/Scripts/WaterMeter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMeter : MonoBehaviour {
+
+    // Use this for initialization
+    public float objScale = 1;
+
+
+    void OnGUI()
+    {
+        objScale = GUI.VerticalSlider(new Rect(100, 100, 100, 90), objScale, 100f, 1);
+
+        if (GUI.changed)
+        {
+            transform.localScale = new Vector3(4, objScale, 1);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
